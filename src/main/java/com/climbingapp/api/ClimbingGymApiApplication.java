8f06b56e5d0df2349533eead6e37c45a2aec5862
Juanjo M. <60217633+juanjomo4@users.noreply.github.com>
2026-01-22 16:58:28 +0100

    Cambios menores

--- src/main/java/com/climbingapp/api/ClimbingGymApiApplication.java
+++ src/main/java/com/climbingapp/api/ClimbingGymApiApplication.java
@@ -5,9 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class ClimbingGymApiApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(ClimbingGymApiApplication.class, args);
-	}
-
+    public static void main(String[] args) {
+        SpringApplication.run(ClimbingGymApiApplication.class, args);
+    }
 }
